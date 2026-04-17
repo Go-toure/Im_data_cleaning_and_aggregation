2414f0da957a81c3a49ef93ec15b10d6a432f392
Go-toure <gorguiba1@gmail.com>
2026-04-17 16:30:16 +0100

    Auto commit: 2026-04-17 16:30

--- raw_data_inspection.R
+++ raw_data_inspection.R
@@ -3,22 +3,25 @@ library(stringr)
 library(qs)
 
 # setwd("C:/Users/TOURE/Mes documents/REPOSITORIES/IM_raw_data/IM_raw/") 6427  8587
-setwd("C:/Users/TOURE/Documents/PADACORD/IM/")
+setwd("C:/Users/TOURE/Documents/PADACORD/LQAS/")
 
-input_file  <- "8587.rds" 
+# data <- read_csv(input_file, show_col_types = FALSE)
+# rds_file <- "C:/Users/TOURE/Documents/PADACORD/IM/7178.rds"
+
+input_file  <- "4500.rds" 
 # input_folder <- "C:/Users/TOURE/Documents/PADACORD/IM/"
 # ============================================================
 # Read raw data
 # ============================================================
 data <- qread(input_file)
-# data <- read_csv(input_file, show_col_types = FALSE)
-# rds_file <- "C:/Users/TOURE/Documents/PADACORD/IM/7178.rds"
+
 
 # file.info(rds_file)$size
 
 # data <- qread(rds_file)
 
 dt <- as.data.table(data)
+
 
 
 # #columns starting with Reason ^NOimmReas NOimmReas_*_other
@@ -58,6 +61,13 @@ reason_cols <- grep("^NOimmReas_.*other$", names(dt), value = TRUE, ignore.case 
 reason_cols
 
 
+#other reasons
+# #columns starting with NOimmReas_ and ending with other
+reason_cols <- grep("Count_HH", names(dt), value = TRUE, ignore.case = TRUE)
+
+reason_cols
+
+
 # distinct values
 all_values_clean <- unique(
   trimws(tolower(unlist(dt[, ..reason_cols])))
@@ -81,3 +91,8 @@ clean_values <- all_values_clean[!grepl("^\\d+$", all_values_clean)]
 clean_values <- clean_values[!grepl("^[a-zA-Z]$", clean_values)]
 clean_values <- clean_values[nchar(clean_values) > 2]
 clean_values
+
+
+
+
+
